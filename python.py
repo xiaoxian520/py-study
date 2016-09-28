f9dc9d276da5a1319aee2ab9edfe57b01ffe625e
xiaoxian <1960588272@qq.com>
2016-09-28 14:56:49 +0800

    a simple py code to play

---
+++ python.py
@@ -0,0 +1,27 @@
+
+#python
+
+import turtle
+
+
+
+def drawSnake(rad, angle, len, neckrad):
+    for i in range(len):
+       turtle.circle(rad, angle)       
+       turtle.circle(-rad, angle)
+       turtle.circle(rad, angle/2)
+       turtle.fd(rad)
+       turtle.circle(neckrad+1, 180)
+       turtle.fd(rad*2/3)
+
+
+def main():
+    turtle.setup(1300, 800, 50, 50)
+    pythonsize=30
+    turtle.pensize(pythonsize)
+    turtle.pencolor("green")
+    turtle.seth(-40)
+    drawSnake(40,80,5,pythonsize/2)
+
+main()
+
